74bcd52d975f535f807e7bf586b8edf19ea6164d
Алексей Соломин <leonidsolomin@yandex.ru>
2024-07-04 21:15:52 +0300

    Внес правки после проверки наставником: 1. в 1-й функции укоротил блок функции; 2. во 2-й функции поменял название служебной переменной на более очевидную; 3. в 3-й функции разделил блоки кода отступами для лучшей читаемости и добавил явное преобразование типа значения "testDataItem"

--- js/functions.js
+++ js/functions.js
@@ -1,31 +1,32 @@
-const checkStringLength = (string, maxLength) => {
-  if (string.length <= maxLength) {
-    return true;
-  }
-  return false;
-};
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
 checkStringLength('проверяемая строка', 20);
 
 const isPalindrome = (originString) => {
-  const transformedOriginString = originString.replaceAll(' ', '').toLowerCase();
-  const reversedString = transformedOriginString.split('').reverse().join('');
-  return (transformedOriginString === reversedString);
+  const mergedLowerCaseString = originString.replaceAll(' ', '').toLowerCase();
+  const reversedString = mergedLowerCaseString.split('').reverse().join('');
+  return (mergedLowerCaseString === reversedString);
 };
 isPalindrome('Лёша на полке клопа нашёл ');
 
 const extractDigits = (testData) => {
   const testDataArray = String(testData).replaceAll(' ', '').split('');
   let resultString = '';
+
   for (const testDataItem of testDataArray) {
     const numberTestDataItem = Number(testDataItem);
+
     if (!Number.isNaN(numberTestDataItem)) {
-      resultString += testDataItem;
+      resultString += testDataItem.toString();
     }
   }
+
   if (resultString) {
     return Number(resultString);
   }
+
   return NaN;
 };
+
 extractDigits('1 кефир, 0.5 батона');
 extractDigits('а я томат');
+extractDigits('0.15  томат');
