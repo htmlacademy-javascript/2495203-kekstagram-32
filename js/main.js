e531395018d463443bfa7cb78ac2be7290dc2325
Алексей Соломин <leonidsolomin@yandex.ru>
2024-07-24 14:23:22 +0300

    Задание 9.13: добавил модуль upload-form.js, в котором реализовал: - открытие/закрытие окна редактирования загружаемой фотографии; - валидацию введенных хештегов и комметария; - применение визуальных эффектов к загружаемой фотографии

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-import {onMiniatureClick} from './image-fullsize-mode.js';
+import { onMiniatureClick } from './image-fullsize-mode.js';
 import { photosDataArray, renderPhotoMiniatures } from './photo-miniatures.js';
+import './upload-form.js';
 
 renderPhotoMiniatures(photosDataArray);
 onMiniatureClick(photosDataArray);
